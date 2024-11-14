d3291e40b14eafc9b744378301d2da732c4bdb6d
sodiq <sa992@student.le.ac.uk>
2024-11-14 16:51:08 +0000

    Backend server created, database created using MongoDB Atlas for remote access

---
+++ PennyUp/backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+
+dotenv.config();
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+//Connect to mongodb database
+mongoose.connect(process.env.mongoURI, { userNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to mongoDB"))
+    .catch(error => console.log(error))
+
+
+//Test
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
+
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+    }); // Starts the server
